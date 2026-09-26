de0fb95cb1edc600c037ef618e4fc4a7b2416bd5
李锐 <romitli@dofun.cn>
2026-09-27 02:31:42 +0800

    修同步时间不一致: 无变化不再推送云端; 历史时间统一校准为服务器时间

--- gem-calculator-sw.js
+++ gem-calculator-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gem-calculator-v43';
+const CACHE_NAME = 'gem-calculator-v44';
 const SW_VERSION = CACHE_NAME.replace('gem-calculator-', '');
 
 self.addEventListener('message', event => {
